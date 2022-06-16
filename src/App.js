664df753ea823170f397b17fd8475321b8c4db0b
Nish <nishtewari2001@gmail.com>
2022-06-15 21:56:45 -0400

    Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ 
+      backgroundImage: `url("https://cutewallpaper.org/21/far-cry-3-blood-dragon-wallpaper/Far-Cry-3-Blood-Dragon-Title-Screen-Background-in-2019-.jpg")` 
+    }}>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
